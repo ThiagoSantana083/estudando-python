162a818f7652282434d34115ac3236fcc7feb7a7
Thiago Santana <thiagosantanara654@gmail.com>
2024-04-23 15:01:53 -0300

    Estudo sobre operadores no python

---
+++ introdução a algoritmos/Operadores.py
@@ -0,0 +1,21 @@
+# Operadores lógicos - and or not
+
+x = 5
+y = 3
+
+print(x>y and y>x)
+print(x>y or y>x)
+print(x==y)
+print(x != y)
+
+# Operadores aritmeticos - + - / // % **
+
+numero1 = 5
+numero2 = 5
+print(numero1 + numero2)
+print(numero1 - numero2)
+print(numero1 * numero2)
+print(numero1 / numero2)
+print(numero1 // numero2)
+print(numero1 % numero2)
+print(numero1 ** numero2)
