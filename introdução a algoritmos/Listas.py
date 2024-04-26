462ecdccbddb1a378e6afd40e0d56e61cc4aa352
Thiago Santana <thiagosantanara654@gmail.com>
2024-04-26 15:41:27 -0300

    Listas em python

---
+++ introdução a algoritmos/Listas.py
@@ -0,0 +1,8 @@
+listaProdutos = ["Notebook", "Smartphone", "iPad", "PS5"]
+precoProdutos = [5500, 1200, 1500, 4500]
+
+for produto in listaProdutos:
+    print(produto)
+
+for preco in precoProdutos:
+    print(preco)
